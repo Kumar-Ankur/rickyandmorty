41218fcc0e4aa2af53eba14f2b4ef329777de6fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 07:55:36 +0000

    Deploy Kumar-Ankur/rickyandmorty to github.com/Kumar-Ankur/rickyandmorty.git:gh-pages

---
+++ precache-manifest.b851eab37bbb73229ede2d9646ad6855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf7498ec4ca6c9c282004ca1d7ee1aa",
+    "url": "/rickyandmorty/index.html"
+  },
+  {
+    "revision": "052adc71b73b9416382f",
+    "url": "/rickyandmorty/static/css/main.b19269c0.chunk.css"
+  },
+  {
+    "revision": "69afd5f84c7c4c461db3",
+    "url": "/rickyandmorty/static/js/2.d28ab0e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rickyandmorty/static/js/2.d28ab0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052adc71b73b9416382f",
+    "url": "/rickyandmorty/static/js/main.d1c2f509.chunk.js"
+  },
+  {
+    "revision": "57564d69cbe11d9cf311",
+    "url": "/rickyandmorty/static/js/runtime-main.9fbc89ef.js"
+  }
+]);
